37fee5aee3d3cd2894a83a258433957ac04f8dce
Alexander Livenets <alivenets@zoho.eu>
2020-03-19 23:55:19 +0100

    First code commit

---
+++ example.cpp
@@ -0,0 +1,16 @@
+#include "example.h"
+
+#include <cmath>
+
+namespace smartgit
+{
+    double calculateSquareRoot(double x)
+    {
+        return 0.0;
+    }
+
+    void printSomething()
+    {
+
+    }
+}
---
+++ example.h
@@ -0,0 +1,12 @@
+
+namespace smartgit
+{
+    template<typename T>
+    T calculateSquare(T x)
+    {
+        return x*x;
+    }
+
+    double calculateSquareRoot(double x);
+    void printSomething();
+}
